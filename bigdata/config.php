63fa1617f290ba29adaf120a8c67caccbd1834e6
puji roby <pujiroby@gmail.com>
2018-12-26 18:21:26 +0700

    Update config.php

--- bigdata/config.php
+++ bigdata/config.php
@@ -1,7 +1,7 @@
 <?php
-    $host = "192.168.22.100";
+    $host = "localhost";
     $username = "root";
-    $password = "4dm1n1234567";
+    $password = "";
     $databasename = "simbada-kotamalang-2017";
     $mysqli = new mysqli( $host, $username, $password, $databasename );
     if ($mysqli->connect_error){
